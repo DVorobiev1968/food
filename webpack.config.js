8aa01d8af863c20bab398ecb8c70b893137f6a3f
DVorobiev1968 <vdv@rdturbo.ru>
2023-05-17 08:16:41 +0500

    Initial repo

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./js/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    liveReload: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+    config.mode = "development";
+  }
+  if (argv.mode === 'production') {
+    config.mode = "production";
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  }
+  return config;
+};
